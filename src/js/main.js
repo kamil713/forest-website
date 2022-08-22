c0a6866abf4afa834b229c2d7ae3d6869117ac31
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-08-22 18:48:56 +0200

    feat: add responsive header

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
-const navMobile = document.querySelector('.nav-mobile');
-const navBtn = document.querySelector('.hamburger');
-// const footerYear = document.querySelector('.footer__year');
+// const navMobile = document.querySelector('.nav-mobile');
+// const navBtn = document.querySelector('.hamburger');
+// // const footerYear = document.querySelector('.footer__year');
 
-const handleNav = () => {
-	navBtn.classList.toggle('is-active');
-	navMobile.classList.toggle('nav-mobile--active');
-};
+// const handleNav = () => {
+// 	navBtn.classList.toggle('is-active');
+// 	navMobile.classList.toggle('nav-mobile--active');
+// };
 
-navBtn.addEventListener('click', handleNav);
+// navBtn.addEventListener('click', handleNav);
 
 // const handleCurrentYear = () => {
 // 	const year = new Date().getFullYear();
@@ -15,3 +15,45 @@ navBtn.addEventListener('click', handleNav);
 // };
 
 // handleCurrentYear();
+
+let $navigationItems; // takes all the navigation items and marks the selected one
+let $navigationLogo; // live above but selected home item ?????
+
+const main = () => {
+	prepareDOMElements();
+	prepareDOMEvents();
+}
+
+const prepareDOMElements = () => {
+	$navigationItems = document.querySelectorAll('.navigation__item');
+	$navigationLogo = document.querySelector('.nav__logo');
+}
+
+const prepareDOMEvents = () => {
+	$navigationItems.forEach(item => {
+		item.addEventListener('click', function () {
+			navItemIsActiveChecker(item);
+		})
+	});
+	$navigationLogo.addEventListener('click', navLogoIsActiveChecker);
+}
+
+const navItemIsActiveChecker = (item) => {
+	$navigationItems.forEach(el => {
+		el.classList.remove('is-active')
+	})
+	item.classList.add('is-active');
+}
+
+const navLogoIsActiveChecker = () => {
+	$navigationItems.forEach(el => {
+		el.classList.remove('is-active')
+	})
+	$navigationItems[0].classList.add('is-active');
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', main);
